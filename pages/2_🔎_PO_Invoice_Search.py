ce235a8886f6fc174cebbaf8c1cfc113a301d070
RJSHS25 <142608744+RJSHS25@users.noreply.github.com>
2026-07-02 08:20:43 +0530

    Add PO Invoice Search page with Streamlit

---
+++ pages/2_🔎_PO_Invoice_Search.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from utils import render_search_page
+
+st.title("🔎 PO Invoice Search")
+
+tab1, = st.tabs(["PO Search Engine"])
+
+with tab1:
+    render_search_page(
+        title="PO Search Engine",
+        csv_file="PO_data.csv",
+        page_name="PO Search"
+    )
